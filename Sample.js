177695d3950623a19fb21b64dc167b4bff48e559
KoshinderGurjar <71639896+KoshinderGurjar@users.noreply.github.com>
2020-11-19 15:57:06 +0530

    Create Sample.js

---
+++ Sample.js
@@ -0,0 +1,49 @@
+
+const webdriver = require('selenium-webdriver');
+ 
+// username: Username can be found at automation dashboard
+const USERNAME = 'koshinder';
+ 
+// AccessKey:  AccessKey can be generated from automation dashboard or profile section
+const KEY = 'U17BIhciO24gmuY8On6bj2PHu1G5I98HH3qwrLoSza8HHPitJE';
+ 
+// gridUrl: gridUrl can be found at automation dashboard
+const GRID_HOST = 'hub.lambdatest.com/wd/hub';
+ 
+function searchTextOnGoogle() {
+    // Setup Input capabilities
+    const capabilities = {
+        platform: 'windows 10',
+        browserName: 'chrome',
+        version: '67.0',
+        resolution: '1280x800',
+        network: true,
+        visual: true,
+        console: true,
+        video: true,
+        name: 'Test 1', // name of the test
+        build: 'NodeJS build' // name of the build
+    }
+ 
+    // URL: https://{username}:{accessKey}@hub.lambdatest.com/wd/hub
+    const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
+ 
+    // setup and build selenium driver object
+    const driver = new webdriver.Builder()
+        .usingServer(gridUrl)
+        .withCapabilities(capabilities)
+        .build();
+ 
+    // navigate to a url, search for a text and get title of page
+    driver.get('https://www.google.com/ncr').then(function() {
+        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
+            driver.getTitle().then(function(title) {
+                setTimeout(function() {
+                    console.log(title);
+                    driver.quit();
+                }, 5000);
+            });
+        });
+    });
+}
+searchTextOnGoogle();
